0d15c5da591817c0157cb4e46afed65f89a867ba
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-03-18 16:50:00 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+function slugify(title) {
+	let titleLower = title.toLowerCase();
+	let changeType = titleLower.split(" ");
+	let changeToString = changeType.join("-")
+	return changeToString;
+}
+
+
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
