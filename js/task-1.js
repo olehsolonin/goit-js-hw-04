5d6d7681b44a80a7f509632a1971f92cf721de49
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-03-18 20:04:26 +0200

    HW4

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,33 @@
-function slugify(title) {
-	let titleLower = title.toLowerCase();
-	let changeType = titleLower.split(" ");
-	let changeToString = changeType.join("-")
-	return changeToString;
+function isEnoughCapacity(products, containerSize) {
+	let values = Object.values(products);
+	let total = 0;
+
+
+	for (let product in values) {
+		total = total + product;
+	}
+	if (total >= containerSize) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 
 
+console.log(
+	isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+	isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+	isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+	isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
